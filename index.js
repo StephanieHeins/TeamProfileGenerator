e38641e214de1cd93e31e50b39804dbfe3b94655
Steph Heins <stephanie_heins@yahoo.com>
2021-10-28 20:56:30 -0700

    Added initialize function, manager questions, menu list, menu function - to index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-// Other files 
-
-
 // Node modules
+const fs = require('fs');
+const inquirer = require('inquirer');
 
+// Other Files
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const generateHTML = require('./src/generateHTML');
 
 // Engineer Questions 
 
@@ -10,7 +14,65 @@
 // Intern Questions 
 
 
-// Menu - Engineer, Inrern, Done 
+// Menu List - Engineer, Intern, Done 
+const menuList = [
+    {
+        type: "list",
+        name: "employeeType",
+        message: "Please select an employee to add to your list:",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Done"
+        ],
+    },
+]
+
+// Menu Function
+function menu() {
+    inquirer.prompt(menuList).then((answers) => {
+        switch (answers.employeeType) {
+            case "Engineer":
+                newEngineer();
+                break;
+            case "Intern":
+                newIntern();
+                break;
+            case "Done":
+                break;
+        };
+    })
+}
 
 
 // Initialize Program - Manager Questions 
+function init() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Manager name:'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Employee ID:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Email:'
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'Office Number:'
+        }
+    ]).then(({ name, id, email, office }) => {
+        // const manage = new Manager(name, id, email, office)
+        // employeeArr.push(manage)
+        menu()
+    })
+}
+
+init()
